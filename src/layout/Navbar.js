f8552c312876c92558a7d983d8d99b3921ec197b
zumrudu-anka <osmandurdag@hotmail.com>
2019-05-26 21:54:06 +0300

    modified navbar.js

---
+++ src/layout/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import myprops from 'prop-types'
+
+function Navbar(title) {
+  return (
+    <nav className="navbar-nav navbar expand-lg navbar-dark bg-dark mb-3 p-3">
+      <a href="/" className="navbar-brand">{title}</a>
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item active">
+          <Link to="/" className="nav-link">Home</Link>
+        </li>
+        <li className="nav-item active">
+          <Link to="/add" className="nav-link">Add User</Link>
+        </li>
+        <li className="nav-item active">
+          <Link to="/github" className="nav-link">Project Files</Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+Navbar.propTypes = {
+    title:myprops.string.isRequired
+}
+Navbar.defaultProps = {
+    title: "Default App"
+}
+export default Navbar;
